8f35407bb0b8598c4bf047f2d75a6b20a25d8f26
ashraful-islam-siam <ashrafullislam.dev1@gmail.com>
2024-04-27 09:06:20 +0600

    Working on HTMl

---
+++ script.js
@@ -0,0 +1,37 @@
+const products = {
+    "woman jacket": [{
+        name: "Yellow Coat Jacket",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, quaerat.",
+        price: 234
+    },
+    {
+        name: "Ladies Jacket",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, quaerat.",
+        price: 234
+    },
+    {
+        name: "Woman Leather Jacket",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, quaerat.",
+        price: 234
+    }
+    ],
+    "man jacket": [
+        {
+            name: "Snowboard Jacket Mens",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, quaerat.",
+            price: 234
+        },
+
+        {
+            name: "Man Leather Jacket",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, quaerat.",
+            price: 234
+        },
+
+        {
+            name: "Man Casual Jacket",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, quaerat.",
+            price: 234
+        }
+    ]
+}
